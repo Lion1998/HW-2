5cc520aa78da346ef5e84c9913b45ba39bffe2d2
לאון <@DESKTOP-S7L8JD7>
2022-09-06 16:07:56 +0300

    Add project files.

---
+++ HW 2/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("____________targil 1____________");
+string a1 = Console.ReadLine(); 
+string a2 = Console.ReadLine();
+Console.WriteLine("a1 = "+a1 +" "+"a2 = "+  a2);
+Console.WriteLine("____________targil 2____________");
+Console.WriteLine("pls enter you year");
+string year1 = Console.ReadLine();
+Console.WriteLine(year1);
+Console.WriteLine("____________targil 3____________");       
+Console.WriteLine("enter your name");
+string name =Console.ReadLine();
+Console.WriteLine("enter your last name");
+string lastName = Console.ReadLine();
+Console.WriteLine("Ty "+ name + " "+ lastName);
+Console.WriteLine("____________targil 4____________");
+Console.WriteLine("# hackeru Shop #");
+Console.WriteLine("please enter your choice,which product wold you like to buy?");
+Console.WriteLine("Milk");
+Console.WriteLine("Chocolate");
+Console.WriteLine("Apple");
+string item = Console.ReadLine();   
+Console.WriteLine("Thank you for buying "+item);
+Console.WriteLine("____________targil 5____________");
+Console.WriteLine("please enter your day");
+string dd = Console.ReadLine();
+Console.WriteLine("please enter your month");
+string mm = Console.ReadLine();
+Console.WriteLine("please enter your year");
+string yy = Console.ReadLine();
+Console.WriteLine(dd + "." + mm + "." + yy);
+Console.WriteLine(dd + "-" + mm + "-" + yy);
+Console.WriteLine(dd + "/" + mm + "/" + yy);
+Console.WriteLine("____________targil 6____________");
+string year = Console.ReadLine();
+int year2 = Convert.ToInt32 (year);
+Console.WriteLine(year);
+
+
+
+
+
+
+
